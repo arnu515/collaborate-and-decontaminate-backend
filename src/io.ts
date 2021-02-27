ba2efae9e88e12aa1e70820c4ea307db4baea663
arnu515 <arnu5152@gmail.com>
2021-02-27 12:46:08 +0530

    add options arg

--- src/io.ts
+++ src/io.ts
@@ -1,6 +1,6 @@
-import { Server } from "socket.io";
+import { Server, ServerOptions } from "socket.io";
 import type { Server as HTTPServer } from "http";
 
-export default (server: HTTPServer) => {
-    return new Server(server);
+export default (server: HTTPServer, options?: Partial<ServerOptions>) => {
+    return new Server(server, options);
 };
